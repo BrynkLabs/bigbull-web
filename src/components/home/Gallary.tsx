ae3d490f5d9cba0b89780921ca99b02a114ac10c
navneetco0 <navneet.dev3@gmail.com>
2025-09-27 02:49:32 +0530

    Fixes: Lint in Gallary

--- src/components/home/Gallary.tsx
+++ src/components/home/Gallary.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useEffect, useState, useRef } from "react";
+import React from "react";
 import Marquee from "react-fast-marquee";
 
 const images: string[] = [
